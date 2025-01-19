0038ac751e2329d3726d2033001333156e47bc43
Anuj Pardeshi <anujpardeshi500@gmail.com>
2025-01-19 11:58:17 +0530

    Add new files

---
+++ src/form.jsx
@@ -0,0 +1,13 @@
+function handleFormSubmit(event){
+    event.preventDefault();
+    console.log("form was submit");
+}
+
+export default function Form(){
+    return(
+        <form>
+            <input type="text" placeholder="Write something" />
+            <button onClick={handleFormSubmit}>Submit</button>
+        </form>
+    )
+}
